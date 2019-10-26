d6b63bf8a23c6c337922e5b5532a38267c2482b3
david <lin.dewei1@gmail.com>
2019-10-26 16:30:25 +0800

    Add comments to admin panel and updated requirements.txt

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,19 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    #fields to include
+    post = models.ForeignKey(Post, related_name='comments',on_delete=models.CASCADE)
+    user = models.CharField(max_length=40) #get User's name
+    email = models.EmailField(max_length=80)
+    cmnt = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    isApproved = models.BooleanField(default=False)
+
+    def approved(self):
+        self.approved = True
+        self.save()
+
+    def __str__(self):
+        return self.user
