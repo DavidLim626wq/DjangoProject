8e45955fd8a847a5d4b909219361448293d1fc9b
david <lin.dewei1@gmail.com>
2019-11-10 01:52:34 +0800

    - Profile/Pic in Admin Panel, - Fixed appearance of forms

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,6 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    verification = forms.IntegerField(required=True)
 
     class Meta:
         model = User
